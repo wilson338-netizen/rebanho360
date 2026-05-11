81487e86b8c91b589698adf5d04538bd9cfa333c
Wilson da Silva <ws338@hotmail.com>
2026-05-11 16:11:36 -0300

    fix cors final web

--- api_main.py
+++ api_main.py
@@ -35,18 +35,10 @@ app = FastAPI(title="Rebanho360 API FINAL")
 
 
 
-origins = [
-    "http://localhost:3000",
-    "http://localhost:5000",
-    "http://127.0.0.1:5000",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 🔥 NÃO usar "*"
-    allow_credentials=True,
+    allow_origins=["*"],  # 🔥 libera tudo (modo dev)
+    allow_credentials=False,  # 🔥 IMPORTANTE
     allow_methods=["*"],
     allow_headers=["*"],
 )
